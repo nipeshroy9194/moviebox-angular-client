9aa14f78407aaba41f0ec2303b37bd4336a312ea
Kumar Baibhav <baibhav.k@husky.neu.edu>
2019-08-11 20:54:20 -0400

    Added init values in fetch calls to use cookies

--- src/app/services/UserServiceClient.ts
+++ src/app/services/UserServiceClient.ts
@@ -7,11 +7,15 @@ export class UserServiceClient {
     fetch(' http://localhost:3000/api/user-login', {
       method: 'POST',
       body: JSON.stringify(loginCredentials),
+      credentials: 'include',
       headers : {
         'content-type': 'application/json'
       }}).then(response => response.json(), error => error.status)
 
   findUserById = (userId) =>
-    fetch('http://localhost:3000/api/user/' + `${userId}`)
+    fetch('http://localhost:3000/api/user/' + `${userId}`,
+      {
+        credentials: 'include'
+      })
       .then(response => response.json(), error => error.status)
 }
