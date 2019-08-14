a681c0daa9a77e3da2f89300d55d5a823e3d9918
Nipesh Roy <roy.nip@husky.neu.edu>
2019-08-14 18:23:28 -0400

    Changed baseNodeURL to Heroku instead of LocalHost

--- src/app/services/AppGlobals.ts
+++ src/app/services/AppGlobals.ts
@@ -2,5 +2,5 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AppGlobals {
-  readonly baseNodeUrl: string = 'http://localhost:3000/';
+  readonly baseNodeUrl: string = 'https://project-server-node.herokuapp.com/';
 }
