6aa33eb4cbd59a506aee72a1c874d427b873047a
Nipesh Roy <roy.nip@husky.neu.edu>
2019-08-14 22:23:48 -0400

    Remove password limit on login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -20,7 +20,7 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required],
-      password: ['', Validators.required, Validators.minLength(2)]
+      password: ['', Validators.required]
     });
   }
 
